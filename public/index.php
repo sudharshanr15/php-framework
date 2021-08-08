cc56998234344d55cf7522c56e28532c5bfc60f1
sudharshan <sudharshanrms2004@gmail.com>
2021-08-08 09:12:26 +0530

    Integrated core Application class

--- public/index.php
+++ public/index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo "Hello world";
+require_once "./../vendor/autoload.php";
+
+use app\core\Application;
+
+$app = new Application();
+
+$app->router->get("/", "home");
+$app->router->post("/", "home");
+
+$app->run();
